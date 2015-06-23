1d692f9175d958624518eb4d1d4ae78973194262
Daniel Chen <Maveri2k@yahoo.com>
2015-06-23 09:59:58 -0400

    all temp convert functions

--- src/temperature.py
+++ src/temperature.py
@@ -7,6 +7,16 @@ def f_to_k(temp):
     converted = ((temp - 32) * (5 / 9.0)) + 273.15
     return converted
 
+def k_to_c(temp):
+    return temp - 273.15
+
+def f_to_c(temp):
+    temp_k = f_to_k(temp)
+    result = k_to_c(temp_k)
+    return result
+
 
 print(f_to_k(32))
 print(f_to_k(212))
+print(k_to_c(273.15))
+print(f_to_c(32))
