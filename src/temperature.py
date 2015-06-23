624c9791f2786588cc9ef4903dd7d1930fd41b04
Daniel Chen <Maveri2k@yahoo.com>
2015-06-23 09:44:58 -0400

    add bad temp f_to_k function

---
+++ src/temperature.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python
+
+"""This is a python module that converts temperatures
+"""
+
+def f_to_k(temp):
+    converted = ((temp - 32) * (5 / 9)) + 273.15
+    return converted
+
+
+print(f_to_k(32))
+print(f_to_k(212))
